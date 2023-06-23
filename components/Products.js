a079551a76aaa602552d4a39d0fb67f9693f9ddb
MoazMirza-13 <moazmirza13@gmail.com>
2023-06-23 19:23:26 +0500

    .filter

--- components/Products.js
+++ components/Products.js
@@ -6,41 +6,32 @@ import ModernSofa from "./ModernSofa";
 import DecentTable from "./DecentTable";
 import SimpleSolidSofa from "./SimpleSolidSofa";
 export default function Products({ activeCategory }) {
+  const products = [
+    { category: "Bedroom", component: BedRoom },
+    { category: "Modern Bedroom", component: BedRoom },
+    { category: "Kitchen", component: Kitchen },
+    { category: "Small Kitchen", component: Kitchen },
+    { category: "Family Kitchen", component: Kitchen },
+    { category: "Family Hall", component: FamilyHall },
+    { category: "T.V Hall", component: FamilyHall },
+    { category: "Main Hall", component: FamilyHall },
+    { category: "Modern Sofa", component: ModernSofa },
+    { category: "Comfy Sofa", component: ModernSofa },
+    { category: "Decent Table", component: DecentTable },
+    { category: "Wide Table", component: DecentTable },
+    { category: "Luna Table", component: DecentTable },
+    { category: "Simple Solid Sofa", component: SimpleSolidSofa },
+    { category: "Simple Soft Sofa", component: SimpleSolidSofa },
+  ];
+  const filteredProducts = products.filter(
+    (product) => product.category === activeCategory
+  );
+
   const renderProduct = () => {
-    switch (activeCategory) {
-      case "Bedroom":
-        return <BedRoom />;
-      case "Modern Bedroom":
-        return <BedRoom />;
-      case "Kitchen":
-        return <Kitchen />;
-      case "Small Kitchen":
-        return <Kitchen />;
-      case "Family Kitchen":
-        return <Kitchen />;
-      case "Decent Table":
-        return <DecentTable />;
-      case "Wide Table":
-        return <DecentTable />;
-      case "T.V Hall":
-        return <FamilyHall />;
-      case "Main Hall":
-        return <FamilyHall />;
-      case "Family Hall":
-        return <FamilyHall />;
-      case "Modern Sofa":
-        return <ModernSofa />;
-      case "Luna Table":
-        return <DecentTable />;
-      case "Comfy Sofa":
-        return <ModernSofa />;
-      case "Simple Solid Sofa":
-        return <SimpleSolidSofa />;
-      case "Simple Soft Sofa":
-        return <SimpleSolidSofa />;
-      default:
-        return <BedRoom />;
-    }
+    return filteredProducts.map((product) => {
+      const ProductComponent = product.component;
+      return <ProductComponent key={product.category} />;
+    });
   };
   return (
     <>
